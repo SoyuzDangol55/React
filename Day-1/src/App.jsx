7a94da5dfc8e3de820fae924fa66412d3118a6c2
SoyuzDangol <dangolsoyuz@gmail.com>
2026-05-29 08:03:00 +0545

    Feat:Add keyvalue arr

--- Day-1/src/App.jsx
+++ Day-1/src/App.jsx
@@ -1,7 +1,8 @@
 import Header from './Header.jsx'
 import Footer from './Footer.jsx'
-import Food from './Food.jsx'
-
+import Food from './Food.jsx' //While importing the default export there is no need of specific name llike
+//import Food from './Food.jsx' // here instead of Food i can write Flower anything but in below one
+import {Listname} from './Food.jsx' // here we must use exact name inside {}
 function App() {
   return (
     <>
@@ -9,6 +10,7 @@ function App() {
      <hr />
      <Food />
      <Footer/>
+      <Listname/>
     </>
   );
 }
--- Day-1/src/Food.jsx
+++ Day-1/src/Food.jsx
@@ -3,15 +3,47 @@ function Food(){
     const food1 = "Pizza";
     const food2 = "Momo";
     const food3 = "Fried Rice"
+    const name = <h1>Soyuz Dangol</h1>
 
     return(
+        // It returns only one parent element  
+        // below <> </> are fragements
+        <>
         <ul>
             <li>Apple</li>
             <li>{food1}</li>
             <li>{food3.toUpperCase()}</li>
             <li>{food2.toLowerCase()}</li>
         </ul>
+
+       
+
+        <h1>name is : {name}</h1>
+        </>
     );
 }
 
-export default Food
+
+function Listname(){
+    const arr = [
+        {Name:"Dikshya Maharjan"},
+        {Name:"Zazzzz Maharjan"},
+        {Name:"Kanxi Awale"}
+    ];
+    arr.push({Name : "SoyuzDangol" });
+    return(
+        <>
+            <h3>Name: {arr[3].Name} </h3>
+            <h3>Name: {arr[0].Name}</h3>
+            <h3>Name: {arr[1].Name}</h3>
+            <h3>Name: {arr[2].Name}</h3>   
+
+        <p>Hello This is Soyuz Dangol .Trying React Second Time and is interesting . </p>
+        </>
+    );
+}
+
+
+//the file can have only one export
+export default Food //only one default export per file
+ export { Listname } //You can have multiple ones of this 
