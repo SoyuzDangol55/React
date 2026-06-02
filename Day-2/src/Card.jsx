d60b496e7545c880f14de2a5440a12fa50411700
SoyuzDangol <dangolsoyuz@gmail.com>
2026-06-02 23:19:42 +0545

    Feat:Add Card

---
+++ Day-2/src/Card.jsx
@@ -0,0 +1,11 @@
+function Card(){
+    return(
+        <div className="Card">
+            <img src="favicon.svg" alt="image" />
+            <h2>Soyuz Dangol</h2>
+            <p>Currently learning React</p>
+        </div>
+    );
+}
+
+export default Card;
