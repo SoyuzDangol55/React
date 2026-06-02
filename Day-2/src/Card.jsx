268c17ff1080c1d0f38f3fa28d5019886f3d40a0
SoyuzDangol <dangolsoyuz@gmail.com>
2026-06-02 23:34:32 +0545

    Style:Add style to the elements

--- Day-2/src/Card.jsx
+++ Day-2/src/Card.jsx
@@ -3,8 +3,8 @@ function Card(){
     return(
         <div className="Card">
             <img src={twefeimage} alt="image" />
-            <h2>Soyuz Dangol</h2>
-            <p>Currently learning React</p>
+            <h2 className='Name'>Soyuz Dangol</h2>
+            <p className='description'>Currently learning React</p>
         </div>
     );
 }
