18dfb2730da070e7660441449825a1069bd97778
SoyuzDangol <dangolsoyuz@gmail.com>
2026-06-10 19:03:07 +0545

    Feat:Add Conditional Rendering and Style

--- Day-4/src/UserGreeting.jsx
+++ Day-4/src/UserGreeting.jsx
@@ -1,9 +1,9 @@
-function UserGreeting(props){
- 
-return props.isLoggedIn ? (
-    <h2 className="welcome-msg">welcome {props.username}</h2>
-):(
-    <h2 className="login-prompt">Please Log-in</h2>  
-);
+function UserGreeting(props) {
+
+    return props.isLoggedIn ? (
+        <h2 className="welcome-msg">welcome {props.username}</h2>
+    ) : (
+        <h2 className="login-prompt">Please Log-in</h2>
+    );
 }
 export default UserGreeting;
