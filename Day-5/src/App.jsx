354ceba727e2258f68620b0fb3b165b893ab0ac8
SoyuzDangol <dangolsoyuz@gmail.com>
2026-06-11 17:01:03 +0545

    Feat:Add Props and display the content

--- Day-5/src/App.jsx
+++ Day-5/src/App.jsx
@@ -1,9 +1,23 @@
 import List from './List.jsx'
 
 function App() {
+   const fruits = [{id:1, name:"Apple",calories:95},
+        {id:2, name:"Mango",calories:45},
+        {id:3, name:"Banana",calories:105},
+        {id:4, name:"Coconut",calories:159}];
+
+
+
+        const vegetables = [{id:5, name:"potatoes",calories:110},
+        {id:6, name:"carrots",calories:25},
+        {id:7, name:"corn",calories:63},
+        {id:8, name:"brocoli",calories:50}];
+
   return(
     <>
-    <List/>
+    <List items = {fruits} category="fruits"/>
+    <List items={vegetables} category="vegetables"/>
+
     </>
   );
 }
--- Day-5/src/List.jsx
+++ Day-5/src/List.jsx
@@ -1,19 +1,25 @@
-function List(){
-    const fruits = [{id:1, name:"Apple",calories:95},
-        {id:2, name:"Mango",calories:45},
-        {id:3, name:"Banana",calories:105},
-        {id:4, name:"Coconut",calories:159}];
+function List(props){
+    // const fruits = [{id:1, name:"Apple",calories:95},
+    //     {id:2, name:"Mango",calories:45},
+    //     {id:3, name:"Banana",calories:105},
+    //     {id:4, name:"Coconut",calories:159}];
 
         //Filtering the Array
         // const lowCaloriesfoods = fruits.filter(fruits => fruits.calories<100);  
+        //const highcalfruits = fruits.filter(fruits => fruits.calories >=100);
 
-        const highcalfruits = fruits.filter(fruits => fruits.calories >=100);
+        const category = props.category;
+        const itemList = props.items;
+        
 
 
-    const listItems = highcalfruits.map(highcalfruits => <li key={highcalfruits.id}>{highcalfruits.name}: {highcalfruits.calories}</li>);
+    const listItems = itemList.map(itemList => <li key={itemList.id}>{itemList.name}: {itemList.calories}</li>);
 
 
 
-    return(<ol>{listItems}</ol>);
+    return(<>
+    <h3>{category}</h3>
+    <ol>{listItems}</ol>
+        </>);
 }
 export default List;
