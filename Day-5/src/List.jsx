305bf918240cb34bc26f4522a32b6a289465d1fa
SoyuzDangol <dangolsoyuz@gmail.com>
2026-06-11 16:45:05 +0545

    Feat:Add Filter property

--- Day-5/src/List.jsx
+++ Day-5/src/List.jsx
@@ -1,9 +1,15 @@
 function List(){
-    const fruits = [{name:"Apple",calories:95},
-        {name:"Mango",calories:45},
-        {name:"Banana",calories:105},
-        {name:"Coconut",calories:159}];
-    const listItems = fruits.map(fruits => <li>{fruits.name}</li>);
+    const fruits = [{id:1, name:"Apple",calories:95},
+        {id:2, name:"Mango",calories:45},
+        {id:3, name:"Banana",calories:105},
+        {id:4, name:"Coconut",calories:159}];
+
+        //Filtering the Array
+        const lowCaloriesfoods = fruits.filter(fruits => fruits.calories<100);
+    const listItems = lowCaloriesfoods.map(lowCaloriesfoods => <li key={lowCaloriesfoods.id}>{lowCaloriesfoods.name}: {lowCaloriesfoods.calories}</li>);
+
+
+
     return(<ol>{listItems}</ol>);
 }
 export default List;
