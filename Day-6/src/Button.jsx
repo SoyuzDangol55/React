7651e16176a7be38b363e6b4277eab678c3168c1
SoyuzDangol <dangolsoyuz@gmail.com>
2026-06-12 18:41:31 +0545

    Feat:Add Button2

--- Day-6/src/Button.jsx
+++ Day-6/src/Button.jsx
@@ -1,8 +1,27 @@
 function Button(){
     const handleClick = function ouch () { console.log("Ouch")};
 
+
+    let count = 0;
+    const handleClick2 = (name2) => {
+        if(count<3){
+            count++;
+            console.log(`${name2} You Clicked me`);
+        }else{
+            console.log(`${name2}Stop Clicking me`);
+        }
+    }
+
+
      function stopClick(name) {console.log(`${name} Stop Clicking me`);}
+
+
+
     return(
-    <button onClick={() => stopClick("Bro")}>Click Me</button>);
+        <>
+    <button onClick={() => stopClick("Bro")}>Click Me</button><br />
+    <button onClick={()=> handleClick2("Soyuz")}>Click me Again</button>
+    </>
+    );
 }
 export default Button
