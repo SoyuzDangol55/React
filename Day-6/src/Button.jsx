c480f0f0b4dba97f265bcf3ffd862b6e8d19f483
SoyuzDangol <dangolsoyuz@gmail.com>
2026-06-12 18:43:18 +0545

    Docs: Format

--- Day-6/src/Button.jsx
+++ Day-6/src/Button.jsx
@@ -1,27 +1,28 @@
-function Button(){
-    const handleClick = function ouch () { console.log("Ouch")};
+function Button() {
+    const handleClick = function ouch() { console.log("Ouch") };
 
 
     let count = 0;
     const handleClick2 = (name2) => {
-        if(count<3){
+        if (count < 3) {
             count++;
             console.log(`${name2} You Clicked me`);
-        }else{
+        } else {
             console.log(`${name2}Stop Clicking me`);
         }
     }
 
 
-     function stopClick(name) {console.log(`${name} Stop Clicking me`);}
+    function stopClick(name) { console.log(`${name} Stop Clicking me`); }
 
 
 
-    return(
+
+    return (
         <>
-    <button onClick={() => stopClick("Bro")}>Click Me</button><br />
-    <button onClick={()=> handleClick2("Soyuz")}>Click me Again</button>
-    </>
+            <button onClick={() => stopClick("Bro")}>Click Me</button><br />
+            <button onClick={() => handleClick2("Soyuz")}>Click me Again</button>
+        </>
     );
 }
 export default Button
