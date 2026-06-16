094fafcd1915c13547432974698cf48b77c271e7
SoyuzDangol <dangolsoyuz@gmail.com>
2026-06-16 07:24:40 +0545

    Initial

--- Show-Case/vite.config.js
+++ Show-Case/vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/React/',           // ← Important for showcase (root)
   plugins: [react()],
 })
