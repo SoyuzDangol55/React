b495b6929ff74bc78d96b9e3351eab45b6e0ef5e
SoyuzDangol <dangolsoyuz@gmail.com>
2026-06-16 07:49:21 +0545

    Feat:Add Day-5 URL

--- Show-Case/src/Buttons.jsx
+++ Show-Case/src/Buttons.jsx
@@ -7,7 +7,7 @@ return(
     <button onClick={()=>window.open("https://day-2-hazel.vercel.app/" , "_blank")}>Day-2</button>
      <button onClick={()=>window.open("https://day-3-dusky.vercel.app/" , "_blank")}>Day-3</button>
       <button onClick={()=>window.open("https://day-4-xi.vercel.app/" , "_blank")}>Day-4</button>
-       <button onClick={()=>window.open("" , "_blank")}>Day-5</button>
+       <button onClick={()=>window.open("https://day-5-iota.vercel.app/" , "_blank")}>Day-5</button>
         <button onClick={()=>window.open("" , "_blank")}>Day-6</button>
          <button onClick={()=>window.open("" , "_blank")}>Day-7</button>
           <button onClick={()=>window.open("" , "_blank")}>Day-8</button>
