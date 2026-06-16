7f1d3eb607d8badabf6ede08caab606531784345
SoyuzDangol <dangolsoyuz@gmail.com>
2026-06-16 07:48:44 +0545

    Feat:Add Day-3,4 URL

--- Show-Case/src/Buttons.jsx
+++ Show-Case/src/Buttons.jsx
@@ -5,12 +5,12 @@ return(
     <h1>Outputs</h1>
    <button onClick={()=>window.open("https://day-1-ten-lovat.vercel.app/" , "_blank")}>Day-1</button>
     <button onClick={()=>window.open("https://day-2-hazel.vercel.app/" , "_blank")}>Day-2</button>
-     <button onClick={()=>window.open("https://day-1-ten-lovat.vercel.app/" , "_blank")}>Day-3</button>
-      <button onClick={()=>window.open("https://day-1-ten-lovat.vercel.app/" , "_blank")}>Day-4</button>
-       <button onClick={()=>window.open("https://day-1-ten-lovat.vercel.app/" , "_blank")}>Day-5</button>
-        <button onClick={()=>window.open("https://day-1-ten-lovat.vercel.app/" , "_blank")}>Day-6</button>
-         <button onClick={()=>window.open("https://day-1-ten-lovat.vercel.app/" , "_blank")}>Day-7</button>
-          <button onClick={()=>window.open("https://day-1-ten-lovat.vercel.app/" , "_blank")}>Day-8</button>
+     <button onClick={()=>window.open("https://day-3-dusky.vercel.app/" , "_blank")}>Day-3</button>
+      <button onClick={()=>window.open("https://day-4-xi.vercel.app/" , "_blank")}>Day-4</button>
+       <button onClick={()=>window.open("" , "_blank")}>Day-5</button>
+        <button onClick={()=>window.open("" , "_blank")}>Day-6</button>
+         <button onClick={()=>window.open("" , "_blank")}>Day-7</button>
+          <button onClick={()=>window.open("" , "_blank")}>Day-8</button>
     </>
 );
 }
